01dcdb404b12c305342f5e6d9cc5e4b9f09eb893
zcsbaimu <3276605184@qq.com>
2024-04-16 17:25:37 +0800

    xswl

--- pyqt/QM_st.py
+++ pyqt/QM_st.py
@@ -1,3 +1,5 @@
+#https://maicss.com/pyqt/v6/menusAndToolbars
+#V_building_0.1
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication,QMenu
 from  PyQt6.QtGui import QIcon,QAction
@@ -7,25 +9,29 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.statusBar().showMessage('状态栏标签')
-        exitAct=QAction(QIcon('exit.png'),'&Exit',self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Exit application')
-        exitAct.triggered.connect(QApplication.instance().quit)
-        menubar=self.menuBar()
-        fileMenu=menubar.addMenu('&File')
+        self.initUI_init()
+        viewenu=self.menubar.addMenu('View')
+        viewStatAct=QAction('View stausbar',self,checkable=True)
+
+    def initUI_init(self):
+        self.statusBar=self.statusBar()
+        self.statusBar.showMessage('状态栏标签')
+        self.exitAct=QAction(QIcon('exit.png'),'&Exit',self)
+        self.exitAct.setShortcut('Ctrl+Q')
+        self.exitAct.setStatusTip('Exit application')
+        self.exitAct.triggered.connect(QApplication.instance().quit)
+        self.menubar=self.menuBar()
+        self.fileMenu=self.menubar.addMenu('&File')
         impMenu=QMenu('Import',self)
         impAct=QAction('Import mail',self)
         impMenu.addAction(impAct)
         newAct=QAction('New',self)
-        fileMenu.addAction(newAct)
-        fileMenu.addAction(exitAct)
-        fileMenu.addMenu(impMenu)
+        self.fileMenu.addAction(newAct)
+        self.fileMenu.addAction(self.exitAct)
+        self.fileMenu.addMenu(impMenu)
         self.setGeometry(300,300,350,250)
         self.setWindowTitle(' this is statusb')
         self.show()
-    def initUI_repeat(self):
-        pass
 def main():
     app=QApplication(sys.argv)
     ex=Example()
